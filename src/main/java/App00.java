3b1da6245f1d3f62dbf2460d8ca3b679e2f4dafd
Kola Ra <santaramus@gmail.com>
2020-01-09 23:12:35 +0300

    first commit

---
+++ src/main/java/App00.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class App00 {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.gecko.driver", "c:\\Users\\Master\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+        WebDriver webDriver = new FirefoxDriver();
+
+        webDriver.get("http://it-ebooks.info/");
+        System.out.println(webDriver.getTitle());
+        System.out.println(webDriver.getCurrentUrl());
+
+        webDriver.findElement(By.id("q")).sendKeys("Automation");
+        webDriver.findElement(By.id("q")).sendKeys(Keys.RETURN);
+
+        webDriver.quit();
+    }
+}
